127c2a1d4324d3b62c309907ecbce82d4b9ba0ed
k1ngS <marcos.beltrao@outlook.pt>
2025-09-12 19:07:21 -0300

    feat(auth) SHA-8 Criar o componente e a rota para pagina de registro

--- shadowchar-frontend/src/app/app.routes.ts
+++ shadowchar-frontend/src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Routes } from '@angular/router';
 import { CharacterList } from './features/character-list/character-list';
 import { CharacterForm } from './features/character-form/character-form';
 import { CharacterDetail } from './features/character-detail/character-detail';
+import { Register } from './features/auth/register/register';
+import { Login } from './features/auth/login/login';
 
 export const routes: Routes = [
+  { path: 'register', component: Register },
+  { path: 'login', component: Login },
+
   { path: 'characters/new', component: CharacterForm },
   { path: 'characters/:id/edit', component: CharacterForm },
   { path: 'characters/:id', component: CharacterDetail },
---
+++ shadowchar-frontend/src/app/features/auth/register/register.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Auth } from '../../../core/auth';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './register.html',
+  styleUrl: './register.scss',
+})
+export class Register {
+  registerForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: Auth,
+    private router: Router,
+  ) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      this.authService.register(this.registerForm.value).subscribe({
+        next: (response) => {
+          console.log('Registro bem-sucedido!', response);
+          this.router.navigate(['/login']);
+        },
+        error: (err) => {
+          console.error('Erro no registro:', err);
+          alert(err.error.message);
+        },
+      });
+    }
+  }
+}
