64e92fd8103dabdb68308bfdb6ca9a458c11ff58
Marcos Beltrão <marcos.beltrao@outlook.pt>
2025-09-15 17:43:28 -0300

    Update shadowchar-frontend/src/app/app.routes.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- shadowchar-frontend/src/app/app.routes.ts
+++ shadowchar-frontend/src/app/app.routes.ts
@@ -5,13 +5,12 @@ import { CharacterDetail } from './features/character-detail/character-detail';
 import { Register } from './features/auth/register/register';
 import { Login } from './features/auth/login/login';
 import { authGuard } from './core/auth-guard';
-import { ProfileComponent } from './features/profile/profile';
+import { Profile } from './features/profile/profile';
 
 export const routes: Routes = [
   { path: 'register', component: Register },
   { path: 'login', component: Login },
-  { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
-
+  { path: 'profile', component: Profile, canActivate: [authGuard] },
   { path: 'characters/new', component: CharacterForm, canActivate: [authGuard] },
   { path: 'characters/:id/edit', component: CharacterForm, canActivate: [authGuard] },
   { path: 'characters/:id', component: CharacterDetail, canActivate: [authGuard] },
